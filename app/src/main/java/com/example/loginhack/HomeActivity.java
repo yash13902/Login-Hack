3b8d0f561e2eed35501aef796f1ffd540bf890e6
yashjasani-1309 <yashbhavin.jasani2020@vitstudent.ac.in>
2021-04-10 05:20:29 +0530

    Initial commit

---
+++ app/src/main/java/com/example/loginhack/HomeActivity.java
@@ -0,0 +1,19 @@
+package com.example.loginhack;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
